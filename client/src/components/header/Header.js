e6c08234904886783222548206d05c2f9ef7610c
Your Name <you@example.com>
2021-10-10 03:48:05 -0700

    navbar spacing

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -96,31 +96,31 @@ const Header = () => {
   // bird40,
   // ];
 
-  function shuffle(array) {
-    let currentIndex = array.length,
-      randomIndex;
+  // function shuffle(array) {
+  //   let currentIndex = array.length,
+  //     randomIndex;
 
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+  //   // While there remain elements to shuffle...
+  //   while (currentIndex !== 0) {
+  //     // Pick a remaining element...
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex--;
 
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
+  //     // And swap it with the current element.
+  //     [array[currentIndex], array[randomIndex]] = [
+  //       array[randomIndex],
+  //       array[currentIndex],
+  //     ];
+  //   }
 
-    return array;
-  }
+  //   return array;
+  // }
 
-  const shuffledBirds = () => {
-    setTimeout(() => shuffle(birds), 1000);
-    return birds
-  };
-  console.log(shuffledBirds);
+  // const shuffledBirds = () => {
+  //   setTimeout(() => shuffle(birds), 1000);
+  //   return birds
+  // };
+  // console.log(shuffledBirds);
 
 
   return (
